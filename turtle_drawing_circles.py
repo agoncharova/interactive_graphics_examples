e531b05aa4d6b58eb7d81a184ac5cfd8b9b7c708
Anastasia Goncharova <stacy.goncharova@icloud.com>
2022-12-11 18:20:22 +0100

    Removed comments

--- turtle_drawing_circles.py
+++ turtle_drawing_circles.py
@@ -5,30 +5,13 @@ tim = t.Turtle()
 tim.shape("turtle")
 t.colormode(255)
 
-
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-
-#RANDOM WALK
+# RANDOM WALK
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
     random_color = (r, g, b)
     return random_color
-# directions = [0, 90, 180, 270]
-#
-# for _ in range(200):
-#     tim.pensize(10)
-#     tim.speed('fast')
-#     tim.color(random_color())
-#     tim.forward(30)
-#     tim.setheading(random.choice(directions))
 
 for _ in range(36):
     tim.speed('fastest')
